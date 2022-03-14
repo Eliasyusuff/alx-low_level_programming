ba97733f50143877ad1fd5e7c69b422dd15c8e06
Eliasyusuff <yusuff.elias@gmail.com>
2022-03-13 21:22:21 -0700

    message

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,7 +9,7 @@ int main(void)
 {
 int n;
 
-for (n = 40; n <= 57; n++)
+for (n = 48; n <= 57; n++)
 {
 putchar(n);
 
@@ -19,8 +19,9 @@ break;
 }
 
 putchar(',');
-putchar(' ');
+putchar (' ');
 }
+
 putchar('\n');
 
 return (0);
